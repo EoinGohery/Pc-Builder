45e6d9b2fa080b597fd6000576ce2619528ef8fc
William <45199953+Willc200@users.noreply.github.com>
2021-04-13 18:02:33 +0100

    Update client with GET requests
    Once program is running, asks user to input number which depending on number selected will return JSON

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -3,10 +3,103 @@ package client
 import (
 	"CS4227/pkg/factory"
 	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
 )
 
 //Run client
 func Run() {
 	fmt.Println("Let's Go Boys!: Frontend")
 	factory.PrintDetails()
+
+	fmt.Println("\nEnter number (1) Cpus (2) Gpus (3) Drivers (4) Motherboards (5) PSU (6) Ram: ")
+	var input int
+	fmt.Scanln(&input)
+	switch input {
+	case 1:
+		resp, err := http.Get("http://localhost:8080/api/parts/getCPUs")
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		defer resp.Body.Close()
+
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		fmt.Printf("%s\n", body)
+	case 2:
+		resp, err := http.Get("http://localhost:8080/api/parts/getGPUs")
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		defer resp.Body.Close()
+
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		fmt.Printf("%s\n", body)
+	case 3:
+		resp, err := http.Get("http://localhost:8080/api/parts/getDrivers")
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		defer resp.Body.Close()
+
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		fmt.Printf("%s\n", body)
+	case 4:
+		resp, err := http.Get("http://localhost:8080/api/parts/getMBDs")
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		defer resp.Body.Close()
+
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		fmt.Printf("%s\n", body)
+	case 5:
+		resp, err := http.Get("http://localhost:8080/api/parts/getPS")
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		defer resp.Body.Close()
+
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		fmt.Printf("%s\n", body)
+	case 6:
+		resp, err := http.Get("http://localhost:8080/api/parts/getRam")
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		defer resp.Body.Close()
+
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		fmt.Printf("%s\n", body)
+	}
 }
